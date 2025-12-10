e8a08934b5be9a61d65ef2cc56ad9d056385f13d
ritikdooly <ritikkumartulbul83753@gmail.com>
2025-12-11 00:18:30 +0530

    modify in problems

--- Multidimensional DP/MinimumPathSum.java
+++ Multidimensional DP/MinimumPathSum.java
@@ -1,5 +1,10 @@
-
+//? https://leetcode.com/problems/minimum-path-sum/description/?envType=study-plan-v2&envId=top-interview-150
 //! 64. Minimum Path Sum
+// Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+// Note: You can only move either down or right at any point in time.
+// Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+// Output: 7
+
 
 public class MinimumPathSum {
 
--- Multidimensional DP/Triangle.java
+++ Multidimensional DP/Triangle.java
@@ -1,5 +1,10 @@
-
+//? https://leetcode.com/problems/triangle/description/?envType=study-plan-v2&envId=top-interview-150
 //! 120 Triangle
+// Given a triangle array, return the minimum path sum from top to bottom.
+// For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+// Example 1:
+// Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+// Output: 11
 
 import java.util.*;
 
