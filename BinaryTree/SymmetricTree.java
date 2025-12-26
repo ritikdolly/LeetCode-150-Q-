408f08aa00df54d4fc35282257a5aa00934e4540
ritikdooly <ritikkumartulbul83753@gmail.com>
2025-12-26 21:05:55 +0530

    modify 101. Symmetric Tree

--- BinaryTree/SymmetricTree.java
+++ BinaryTree/SymmetricTree.java
@@ -1,5 +1,12 @@
+// ? https://leetcode.com/problems/symmetric-tree/description/?envType=study-plan-v2&envId=top-interview-150
 
 // !  101. Symmetric Tree
+// Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+// Example 1:
+
+// Input: root = [1,2,2,3,4,4,3]
+// Output: true
 
 public class SymmetricTree {
 
