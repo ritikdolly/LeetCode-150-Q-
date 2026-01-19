f0214acda3396ceec6bd6dc75356d5e1d0fed74c
ritikdolly <devwithritik200@gmail.com>
2026-01-19 22:30:12 +0530

    modify 88. Merge Sorted Array

--- Arrays/MergeSortedArray.java
+++ Arrays/MergeSortedArray.java
@@ -30,4 +30,19 @@ public class MergeSortedArray {
 
         }
     }
+
+    public static void main(String[] args) {
+        Solution sol = new MergeSortedArray().new Solution();
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+        sol.merge(nums1, m, nums2, n);
+
+        // Print merged array
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+    }
 }
